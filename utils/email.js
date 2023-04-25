cd6383dd2119bf20e4653fb9b08936820614e16a
codemonkey721 <code.monkey721@gmail.com>
2023-04-24 22:16:49 -0700

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,8 @@
 const nodemailer = require('nodemailer');
+const mailgun = require('mailgun-js')({
+  apiKey: 'df0a4ec14c6ab83e35eea5324a93ef6a-70c38fed-ed7ad58a',
+  domain: 'bpltoolshop1-jamesgaylor721.b4a.run'
+});
 // const mailgun = require('mailgun-js')({
 //   apiKey: "5082a622e448e21378aa119ed54ae9aa-181449aa-6fc11782",
 //   domain: "sandbox6bf43c088a914f85afe42c1c00bfd040.mailgun.org"
@@ -14,13 +18,13 @@ const nodemailer = require('nodemailer');
 
 const sendPasswordResetEmail = async (email, token) => {
   console.log(email, token);
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'james.gaylor721@gmail.com',
-        pass: 'Psg2002.415.com'
-    }
-  });
+  // let transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //       user: 'james.gaylor721@gmail.com',
+  //       pass: 'Psg2002.415.com'
+  //   }
+  // });
   
   let mailOptions = {
     from: 'james.gaylor721@gmail.com',
@@ -29,13 +33,13 @@ const sendPasswordResetEmail = async (email, token) => {
     text: 'This is a test email sent using node-mailer.'
   };
   
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-  });
+  // transporter.sendMail(mailOptions, function(error, info){
+  //   if (error) {
+  //       console.log(error);
+  //   } else {
+  //       console.log('Email sent: ' + info.response);
+  //   }
+  // });
   // const resetUrl = `http://localhost:3000/reset-password/${token}`;
   // const data = {
   //   from: 'james.gaylor721@gmail.com',
@@ -44,13 +48,13 @@ const sendPasswordResetEmail = async (email, token) => {
   //   text: 'This is a test email'
   // };
   
-  // mailgun.messages().send(data, (error, body) => {
-  //   if (error) {
-  //     console.error(error);
-  //   } else {
-  //     console.log('Email sent', body);
-  //   }
-  // });
+  mailgun.messages().send(mailOptions, (error, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Email sent', body);
+    }
+  });
 };
 
 module.exports = { sendPasswordResetEmail };
